e675c82b352e6fb780fc4c7b268ed3c125f915e1
Irene Nyakate <inyakate@gmail.com>
2018-11-09 13:33:01 +0300

    Add endpoint for a user to login [Finishes #161825483]

--- api/models.py
+++ api/models.py
@@ -3,3 +3,30 @@ from flask import Flask, request, jsonify, make_response
 
 def get_index_page():
     return "You are most welcome to our home page"
+
+""" This is a global variable called USERS """
+USERS = [{"username": "admin", "password": "admin", "role": "admin"},
+         {"username": "Maxie", "password": "elite", "role": "user"}]
+
+
+class User:
+    """
+    This class defines the user in terms of
+    the username, password and user_role.
+    """
+
+    user_role = ""
+
+    def __init__(self, username, password, role):
+        self.username = username
+        self.password = password
+        self.role = role
+
+    def login(self):
+        """ This method allows the user to login after cross checking their
+            username, password and user role
+        """
+        for user in USERS:
+            if user['username'] == self.username and user['password'] == self.password and user['role'] == self.role:
+                User.user_role = self.role
+        return self.user_role
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, make_response
+from api.models import User
 
 
 app = Flask(__name__)
@@ -9,3 +10,22 @@ def index():
     """ Endpoint to get the index page of the application"""
     my_response = "You are most welcome to our home page"
     return jsonify(my_response), 200
+
+
+@app.route('/auth/login', methods=['POST'])
+def login():
+    """ Endpoint for a user to login """
+    user_data = request.get_json()
+    # get login data from user
+    name = user_data.get('username')
+    password = user_data.get('password')
+    role = user_data.get('role')
+
+    user = User(name, password, role)
+    login_result = user.login()
+
+    if login_result:
+        return jsonify('Welcome {}'.format(login_result)), 200
+    return jsonify({
+                'message': 'Sorry please login with the right credentials'
+                }), 400
