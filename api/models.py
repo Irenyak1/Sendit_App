3f9c0e15788fb7257e45f2ddd6665e2413d54faf
Irene Nyakate <inyakate@gmail.com>
2018-11-11 00:01:51 +0300

    Add endpoint to create order

--- api/models.py
+++ api/models.py
@@ -30,3 +30,55 @@ class User:
             if user['username'] == self.username and user['password'] == self.password and user['role'] == self.role:
                 User.user_role = self.role
         return self.user_role
+
+
+class Order:
+    """
+    This class defines a parcel delivery order in terms of
+    user_id, user_name, contact, order_id, pickup_location, destination, weight, price.
+    """
+
+    ORDERS = []
+    """
+    This is a list that will store all the deliverry orders
+    that will be  created.
+    """
+
+    def __init__(self, user_id, user_name, contact,
+                 pickup_location, destination, weight, price):
+        self.user_id = user_id
+        self.user_name = user_name
+        self.contact = contact
+        self.pickup_location = pickup_location
+        self.destination = destination
+        self.weight = weight
+        self.price = price
+    
+    def create_a_delivery_order(self):
+        """ This method allows a user to create a delivery order.
+            And it also generates the order_id automatically.
+        """
+        if User.user_role == 'user':
+            my_orders = dict
+            if not Order.ORDERS:
+                order_id = 1
+        else:
+            order_id = Order.ORDERS[-1].get('order_id') + 1
+
+        self.my_orders = {
+            'user_id': self.user_id,
+            'user_name': self.user_name,
+            'contact': self.contact,
+            'pickup_location': self.pickup_location,
+            'destination': self.destination,
+            'weight': self.weight,
+            'price': self.price,
+            'order_id': order_id
+        }
+
+        Order.ORDERS.append(self.my_orders)
+
+        return 'You have created a delivery order.'
+
+
+        
--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, make_response
-from api.models import User
+from api.models import User, Order
 
 
 app = Flask(__name__)
@@ -29,3 +29,81 @@ def login():
     return jsonify({
                 'message': 'Sorry please login with the right credentials'
                 }), 400
+
+@app.route('/api/v1/orders', methods=['POST'])
+def create_a_delivery_order():
+    """ Endpoint to create a delivery order by the user """
+    if User.user_role == 'user':
+        order_data = request.get_json()
+        # get sale data
+        user_id = order_data.get('user_id')
+        user_name = order_data.get('user_name')
+        contact = order_data.get('contact')
+        pickup_location = order_data.get('pickup_location')
+        destination = order_data.get('destination')
+        weight = order_data.get('weight')
+        price = order_data.get('price')
+     
+        # validate sale data
+        if not order_data:
+            return jsonify({'message': 'Please fill all the feilds'}), 400
+
+        if not user_id or user_id == "":
+            return jsonify({
+                'message': 'Oops! fill in user_id and should be an integer'
+            }), 400
+
+        if not user_name or user_name == "":
+            return jsonify({
+                'message': 'sorry! user name is required and can not be an empty string.'
+            }), 400
+
+        if not contact or contact == "":
+            return jsonify({
+                'message': 'Please! the contact is required, it should not be blank.'
+            }), 400
+       
+        if type(contact) != int:
+            return jsonify({
+                'message': 'Please the contact should be an integer.'
+            }), 400
+
+        if not pickup_location  or pickup_location == "":
+            return jsonify({
+                'message': 'Please! pickup location is required and can not be an empty string.'
+            }), 400
+        
+        if not destination  or destination == "":
+            return jsonify({
+                'message': 'Please! the destination is required and can not be an empty string.'
+            }), 400
+        
+        if not weight  or weight == "":
+            return jsonify({
+                'message': 'Please! the destination is required and can not be an empty string.'
+            }), 400
+
+        if type(weight) != int:
+            return jsonify({
+                'message': 'Please! the weight should be filled as an integer.'
+            }), 400
+        
+        if not price or price == "":
+            return jsonify({
+                'message': 'Please! the price is required '
+            }), 400
+
+        if type(weight) != int:
+            return jsonify({
+                'message': 'Please! the price is required as an integer.'
+            }), 400
+
+        new_order = Order(user_id, user_name, contact, pickup_location, destination, weight, price)
+        result_create_a_delivery_order = new_order.create_a_delivery_order()
+
+        if result_create_a_delivery_order:
+            return jsonify({'message': 'Sale order created successfully',
+                            'new_order': new_order
+                          }), 201
+
+    return jsonify({'message': 'You can not create a delivery order if you are not a user.'}), 400
